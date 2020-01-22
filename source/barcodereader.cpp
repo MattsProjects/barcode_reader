54b9438997f195ebefba4a6468e6f4ab3323150a
Breit <Matthew.Breit@baslerweb.com>
2020-01-22 15:03:47 -0500

    Completely re-written to remove all processing needs for OpenCV. (Linux will still need it for display) Updated makefile and VS project accordingly. VS Solution file added also.
    Signed-off-by: Breit <Matthew.Breit@baslerweb.com>

--- source/barcodereader.cpp
+++ source/barcodereader.cpp
@@ -1,12 +1,12 @@
-/* PylonSample_Barcode.cpp - mbreit
+/* barcodereader.cpp - mbreit
 
-	Copyright 2017 Matthew Breit <matt.breit@gmail.com>
+	Copyright 2017 - 2019 Matthew Breit <matt.breit@gmail.com>
 
 	Licensed under the Apache License, Version 2.0 (the "License");
 	you may not use this file except in compliance with the License.
 	You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 	Unless required by applicable law or agreed to in writing, software
 	distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,25 +19,25 @@
 	IS OUTSIDE THE SCOPE OF THIS LICENSE.
 
 
- BARCODE SOFTWARE LICENSE
+	BARCODE SOFTWARE LICENSE (zxing)
 	Software License Agreement (BSD License)
 	----------------------------------------
 	Copyright (c) 2013 IDEAL Software GmbH, Neuss, Germany. All rights reserved.
-                              www.IdealSoftware.com
+	www.IdealSoftware.com
 
 	Redistribution and use in source and binary forms, with or without modification,
 	are permitted provided that the following conditions are met:
 
-	* Redistributions of source code must retain the above copyright notice, this 
-	  list of conditions and the following disclaimer.
+	* Redistributions of source code must retain the above copyright notice, this
+	list of conditions and the following disclaimer.
 
 	* Redistributions in binary form must reproduce the above copyright notice, this
-	  list of conditions and the following disclaimer in the documentation and/or
-	  other materials provided with the distribution.
+	list of conditions and the following disclaimer in the documentation and/or
+	other materials provided with the distribution.
 
 	* Neither the name IDEAL Software, nor the names of contributors
-	  may be used to endorse or promote products derived from this software without
-	  specific prior written permission of IDEAL Software.
+	may be used to endorse or promote products derived from this software without
+	specific prior written permission of IDEAL Software.
 
 	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -49,14 +49,8 @@
 	ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+	*/
 
-
-#include <opencv/cv.h> 
-#include <opencv/highgui.h> 
-
-#include <stdio.h>
-#include <iostream>
 
 #include "zxing/LuminanceSource.h"
 #include "zxing/MultiFormatReader.h"
@@ -76,117 +70,124 @@
 #    include <pylon/PylonGUI.h>
 #endif
 
-#define USE_USB
-
-#if defined( USE_1394 )
-// Settings for using Basler IEEE 1394 cameras.
-#include <pylon/1394/Basler1394InstantCamera.h>
-typedef Pylon::CBasler1394InstantCamera Camera_t;
-using namespace Basler_IIDC1394CameraParams;
-#elif defined ( USE_GIGE )
-// Settings for using Basler GigE cameras.
-#include <pylon/gige/BaslerGigEInstantCamera.h>
-typedef Pylon::CBaslerGigEInstantCamera Camera_t;
-using namespace Basler_GigECameraParams;
-#elif defined ( USE_CAMERALINK )
-// Settings for using Basler Camera Link cameras.
-#include <pylon/cameralink/BaslerCameraLinkInstantCamera.h>
-typedef Pylon::CBaslerCameraLinkInstantCamera Camera_t;
-using namespace Basler_CLCameraParams;
-#elif defined ( USE_USB )
-// Settings for using Basler USB cameras.
-#include <pylon/usb/BaslerUsbInstantCamera.h>
-typedef Pylon::CBaslerUsbInstantCamera Camera_t;
-using namespace Basler_UsbCameraParams;
-#else
-#error Camera type is not specified. For example, define USE_GIGE for using GigE cameras.
-#endif
-
 // Namespace for using pylon objects.
 using namespace Pylon;
 
 // Namespace for using cout.
 using namespace std;
 
-using namespace zxing;
-using namespace oned;
-using namespace datamatrix;
-using namespace qrcode;
-using namespace aztec;
-
 // Number of images to be grabbed.
-static const uint32_t c_countOfImagesToGrab = 100;
+static const uint32_t c_countOfImagesToGrab = 1000;
 
-class OpenCVBitmapSource : public LuminanceSource
+class BarcodeReader
 {
 private:
-	cv::Mat m_pImage;
+	class PylonImageSource : public zxing::LuminanceSource
+	{
+	private:
+		Pylon::CPylonImage m_Image;
+
+	public:
+		PylonImageSource(Pylon::CPylonImage &image) : LuminanceSource(image.GetWidth(), image.GetHeight())
+		{
+			m_Image.CopyImage(image);
+		}
+
+		~PylonImageSource()
+		{}
+
+		int getWidth() const { return m_Image.GetWidth(); }
+		int getHeight() const { return m_Image.GetHeight(); }
+
+		zxing::ArrayRef<char> getRow(int y, zxing::ArrayRef<char> row) const //See Zxing Array.h for ArrayRef def
+		{
+			int width_ = getWidth();
+			if (!row)
+				row = zxing::ArrayRef<char>(width_);
+			int yoffset = y * width_;
+			uint8_t* pBuffer = (uint8_t*)m_Image.GetBuffer();
+			for (int x = 0; x < width_; ++x)
+			{
+				row[x] = pBuffer[yoffset + x];
+			}
+
+			return row;
+		}
+
+		zxing::ArrayRef<char> getMatrix() const
+		{
+			int width_ = getWidth();
+			int height_ = getHeight();
+			zxing::ArrayRef<char> matrix = zxing::ArrayRef<char>(width_ * height_);
+			uint8_t* pBuffer = (uint8_t*)m_Image.GetBuffer();
+			for (int y = 0; y < height_; ++y)
+			{
+				int yoffset = y * width_;
+				for (int x = 0; x < width_; ++x)
+				{
+					matrix[yoffset + x] = pBuffer[yoffset + x];
+				}
+			}
+			return matrix;
+		}
+
+		CPylonImage GetImage()
+		{
+			return m_Image;
+		}
+		/*
+		// The following methods are not supported by this demo (the DataMatrix Reader doesn't call these methods)
+		bool isCropSupported() const { return false; }
+		Ref<LuminanceSource> crop(int left, int top, int width, int height) {}
+		bool isRotateSupported() const { return false; }
+		Ref<LuminanceSource> rotateCounterClockwise() {}
+		*/
+	};
+
+	zxing::MultiFormatReader m_reader;
 
 public:
-	OpenCVBitmapSource(cv::Mat &image)
-		: LuminanceSource(image.cols, image.rows)
+	BarcodeReader(){}
+	~BarcodeReader(){}
+
+	struct BRResult
 	{
-		m_pImage = image.clone();
-	}
+		bool BarcodeFound = false;
+		double XLocation = -1;
+		double YLocation = -1;
+		std::string BarcodeData = "";
+		std::string ErrorMessage = "";
+	};
 
-	~OpenCVBitmapSource(){}
-
-	int getWidth() const { return m_pImage.cols; }
-	int getHeight() const { return m_pImage.rows; }
-
-	ArrayRef<char> getRow(int y, ArrayRef<char> row) const //See Zxing Array.h for ArrayRef def
+	BRResult ReadImage(CPylonImage image)
 	{
-		int width_ = getWidth();
-		if (!row)
-			row = ArrayRef<char>(width_);
-		const char *p = m_pImage.ptr<char>(y);
-		for (int x = 0; x<width_; ++x, ++p)
-			row[x] = *p;
-		return row;
-	}
+		BRResult r;
 
-	ArrayRef<char> getMatrix() const
-	{
-		int width_ = getWidth();
-		int height_ = getHeight();
-		ArrayRef<char> matrix = ArrayRef<char>(width_*height_);
-		for (int y = 0; y < height_; ++y)
+		zxing::Ref<PylonImageSource> source(new PylonImageSource(image));
+		zxing::Ref<zxing::Binarizer> binarizer(new zxing::GlobalHistogramBinarizer(source));
+		zxing::Ref<zxing::BinaryBitmap> bitmap(new zxing::BinaryBitmap(binarizer));
+
+		try
 		{
-			const char *p = m_pImage.ptr<char>(y);
-			int yoffset = y*width_;
-			for (int x = 0; x < width_; ++x, ++p)
-			{
-				matrix[yoffset + x] = *p;
-			}
+			zxing::Ref<zxing::Result> result = m_reader.decode(bitmap);
+			r.BarcodeFound = true;
+			r.BarcodeData = result->getText()->getText();
+			zxing::ArrayRef<zxing::Ref<zxing::ResultPoint>> pts = result->getResultPoints();
+			r.XLocation = pts[0]->getX();
+			r.YLocation = pts[0]->getY();
 		}
-		return matrix;
-	}
-	/*
-	// The following methods are not supported by this demo (the DataMatrix Reader doesn't call these methods)
-	bool isCropSupported() const { return false; }
-	Ref<LuminanceSource> crop(int left, int top, int width, int height) {}
-	bool isRotateSupported() const { return false; }
-	Ref<LuminanceSource> rotateCounterClockwise() {}
-	*/
-};
+		catch (zxing::Exception& e)
+		{
+			r.BarcodeFound = false;
+			r.BarcodeData = "";
+			r.XLocation = -1;
+			r.YLocation = -1;
+			r.ErrorMessage = e.what();
+		}
 
-void decode_image(Reader *reader, IplImage *image)
-{
-	try
-	{
-		cv::Mat m = cv::cvarrToMat(image);
-		Ref<OpenCVBitmapSource> source(new OpenCVBitmapSource(m));
-		Ref<Binarizer> binarizer(new GlobalHistogramBinarizer(source));
-		Ref<BinaryBitmap> bitmap(new BinaryBitmap(binarizer));
-		Ref<Result> result(reader->decode(bitmap, DecodeHints(DecodeHints::DEFAULT_HINT)));
-		cout << "Barcode value is: " << result->getText() << endl;
-		//Beep(440, 100);
+		return r;
 	}
-	catch (zxing::Exception& e)
-	{
-		 cerr << "Error: " << e.what() << endl;
-	}
-}
+};
 
 int main(int argc, char* argv[])
 {
@@ -199,73 +200,36 @@ int main(int argc, char* argv[])
 
 	try
 	{
-			
-		CDeviceInfo info;
-		String_t serialNumber = "";
-		cout << "Enter Serial Number (dart only!): ";
-		cin >> serialNumber;
-		info.SetSerialNumber(serialNumber);
-
 		// Create an instant camera object with the camera device found first.
-		Camera_t camera(CTlFactory::GetInstance().CreateFirstDevice(info));
+		CInstantCamera camera(CTlFactory::GetInstance().CreateFirstDevice());
 
 		// Print the model name of the camera.
 		cout << "Using device " << camera.GetDeviceInfo().GetModelName() << endl;
 
 		camera.Open();
-		camera.UserSetSelector.SetValue(UserSetSelector_Default);
-		camera.UserSetLoad.Execute();
+		GenApi::CEnumerationPtr(camera.GetNodeMap().GetNode("UserSetSelector"))->FromString("Default");
+		GenApi::CCommandPtr(camera.GetNodeMap().GetNode("UserSetLoad"))->Execute();
 
-		camera.PixelFormat.SetValue(PixelFormat_Mono8);
-		camera.ExposureAuto.SetValue(ExposureAuto_Continuous);
-		camera.BslBrightness.SetValue(0.4);
-		camera.BslContrast.SetValue(0.4);
+		if (GenApi::IsWritable(camera.GetNodeMap().GetNode("ExposureAuto")))
+			GenApi::CEnumerationPtr(camera.GetNodeMap().GetNode("ExposureAuto"))->FromString("Continuous");
+		if (GenApi::IsWritable(camera.GetNodeMap().GetNode("GainAuto")))
+			GenApi::CEnumerationPtr(camera.GetNodeMap().GetNode("GainAuto"))->FromString("Continuous");
 
-		cout << "Grabbing 100 images to settle autoexposure..." << endl;
-		camera.StartGrabbing(100);
-		while (camera.IsGrabbing())
-		{
-			CGrabResultPtr result;
-			camera.RetrieveResult(1000, result, TimeoutHandling_ThrowException);
-			cout << ".";
-		}
+		if (GenApi::IsWritable(camera.GetNodeMap().GetNode("BslBrightness")))
+			GenApi::CFloatPtr(camera.GetNodeMap().GetNode("BslBrightness"))->SetValue(0.4);
+		if (GenApi::IsWritable(camera.GetNodeMap().GetNode("BslContrast")))
+			GenApi::CFloatPtr(camera.GetNodeMap().GetNode("BslContrast"))->SetValue(GenApi::CFloatPtr(camera.GetNodeMap().GetNode("BslContrast"))->GetMax());
+		if (GenApi::IsWritable(camera.GetNodeMap().GetNode("AutoTargetBrightness")))
+			GenApi::CFloatPtr(camera.GetNodeMap().GetNode("AutoTargetBrightness"))->SetValue(0.5);
 
-		cout << endl << "Starting Barcode Reading..." << endl;
-		camera.TriggerSelector.SetValue(TriggerSelector_FrameStart);
-		camera.TriggerSource.SetValue(TriggerSource_Software);
-		camera.TriggerMode.SetValue(TriggerMode_On);
+		GenApi::CEnumerationPtr(camera.GetNodeMap().GetNode("TriggerSelector"))->FromString("FrameStart");
+		GenApi::CEnumerationPtr(camera.GetNodeMap().GetNode("TriggerSource"))->FromString("Software");
+		GenApi::CEnumerationPtr(camera.GetNodeMap().GetNode("TriggerMode"))->FromString("On");
 
-		int width, height;
-		width = camera.Width.GetValue();
-		height = camera.Height.GetValue();	
+		int64_t width = GenApi::CIntegerPtr(camera.GetNodeMap().GetNode("Width"))->GetValue();
+		int64_t height = GenApi::CIntegerPtr(camera.GetNodeMap().GetNode("Height"))->GetValue();
 
-		IplImage *frame;		
-
-		MultiFormatReader reader;
-
-		IplImage *gray = cvCreateImage(cvSize(width, height), IPL_DEPTH_8U, 1);
-		if (!gray)
-		{
-			fprintf(stderr, "ERROR: allocating image\n");
-			getchar();
-			return -1;
-		}
-
-		// create crop image
-		int target_width = width * 2 / 3;
-		int target_height = height * 2 / 3;
-		int startx = (width - target_width) / 2;
-		int starty = (height - target_height) / 2;
-		IplImage *crop = cvCreateImage(cvSize(target_width, target_height), IPL_DEPTH_8U, 1);
-		if (!crop)
-		{
-			fprintf(stderr, "ERROR: allocating image\n");
-			getchar();
-			return -1;
-		}
-
-		// Create a window in which the captured images will be presented
-		cvNamedWindow("Barcode Reader", CV_WINDOW_AUTOSIZE);
+		BarcodeReader myBarcodeReader;
 
 		// The parameter MaxNumBuffer can be used to control the count of buffers
 		// allocated for grabbing. The default value of this parameter is 10.
@@ -291,66 +255,39 @@ int main(int argc, char* argv[])
 			// Image grabbed successfully?
 			if (ptrGrabResult->GrabSucceeded())
 			{
+				cout << endl;
 				cout << "Image Received: " << ptrGrabResult->GetBlockID() << endl;
 
-				Pylon::CImageFormatConverter fc; // for converting the image to/from different formats
-				fc.OutputPixelFormat = Pylon::PixelType_BGR8packed; // OpenCV uses BGR8packed format
-				Pylon::CPylonImage pylonImage; // the original image container for the grab result.
-				// convert the image to BGR8packed
-				fc.Convert(pylonImage, ptrGrabResult);
-				cv::Mat cv_img;
-				cv_img = cv::Mat(ptrGrabResult->GetHeight(), ptrGrabResult->GetWidth(), CV_8UC3, (uint8_t*)pylonImage.GetBuffer());
-				frame = new IplImage(cv_img);
+				// use pylon to convert whatever format the incoming image is to Mono8
+				Pylon::CImageFormatConverter fc;
+				fc.OutputPixelFormat = Pylon::PixelType_Mono8;
+				Pylon::CPylonImage pylonImage;
+				if (fc.ImageHasDestinationFormat(ptrGrabResult))
+					pylonImage.AttachGrabResultBuffer(ptrGrabResult);
+				else
+					fc.Convert(pylonImage, ptrGrabResult);
 
-				// CV_RGB2GRAY: convert RGB image to grayscale
-				cvCvtColor(frame, gray, CV_RGB2GRAY);
+				Pylon::DisplayImage(0, pylonImage);
 
-				// crop image
-				cvSetImageROI(gray, cvRect(startx, starty, target_width, target_height));
-				cvCopy(gray, crop, NULL);
-				cvResetImageROI(gray);
+				BarcodeReader myBarcodeReader;
+				BarcodeReader::BRResult myResult;
 
-				// display resized original and paint crop rectangle
-				float scale = 0.8f;
-				IplImage *resized = cvCreateImage(cvSize((int)(width * scale), (int)(height * scale)), gray->depth, gray->nChannels);
-				cvResize(gray, resized);
+				myResult = myBarcodeReader.ReadImage(pylonImage);
 
-				// paint target crop rectangle
-				int sstartx = (int)(startx * scale);
-				int starget_width = (int)(target_width * scale);
-				int sstarty = (int)(starty * scale);
-				int starget_height = (int)(target_height * scale);
-				for (int x = sstartx; x < sstartx + starget_width; x++)
+				if (myResult.BarcodeFound == true)
 				{
-					resized->imageData[sstarty * resized->widthStep + x] = 0;
-					resized->imageData[(sstarty + starget_height) * resized->widthStep + x] = 0;
+					cout << "Barcode Found: " << endl;
+					cout << " Location : " << "X: " << myResult.XLocation << " Y: " << myResult.YLocation << endl;
+					cout << " Data     : " << myResult.BarcodeData << endl;
 				}
-
-				for (int y = sstarty; y < sstarty + starget_height; y++)
-				{
-					resized->imageData[y * resized->widthStep + sstartx] = 0;
-					resized->imageData[y * resized->widthStep + sstartx + starget_width] = 0;
-				}
-
-				cvShowImage("Barcode Reader", resized);
-				cvWaitKey(1);
-				cvReleaseImage(&resized);
-
-				// decode barcode
-				decode_image(&reader, crop);
-
+				else
+					cout << "Barcode Not Found: " << myResult.ErrorMessage << endl;
 			}
 			else
 			{
 				cout << "Error: " << ptrGrabResult->GetErrorCode() << " " << ptrGrabResult->GetErrorDescription() << endl;
 			}
 		}
-
-		cvDestroyWindow("Barcode Reader");
-		cvReleaseImage(&gray);
-		cvReleaseImage(&crop);
-
-		camera.Close();
 	}
 	catch (GenICam::GenericException &e)
 	{
